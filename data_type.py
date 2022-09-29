7b70c3e39c5b7e2495aa7c78c62c4b064fa45c24
maivudhsp <maivanphuongvu.dhsp@gmail.com>
2022-09-29 09:07:45 +0700

    first commit

---
+++ data_type.py
@@ -0,0 +1,10 @@
+a = True
+x = 1
+y = 1.5
+z = "hello"
+
+
+print(str(a) + " is of the data type " + str(type(a)))
+print(str(x) + " is of the data type " + str(type(x)))
+print(str(y) + " is of the data type " + str(type(y)))
+print(str(z) + " is of the data type " + str(type(z)))
